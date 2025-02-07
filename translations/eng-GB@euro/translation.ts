edc278c2b196893d3a85a3ad1c4dcede94e4b4b2
Luca Realdi <luca.realdi@opencontent.it>
2025-02-07 01:11:54 +0100

    Add eng-GB@euro

---
+++ translations/eng-GB@euro/translation.ts
@@ -0,0 +1,111 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+  <context>
+    <name>opendata_forms</name>
+    <message>
+      <source>Store</source>
+      <translation>Save</translation>
+    </message>
+    <message>
+      <source>Cancel</source>
+      <translation>Undo</translation>
+    </message>
+    <message>
+      <source>Loading...</source>
+      <translation>Loading...</translation>
+    </message>
+    <message>
+      <source>Cancel deletion</source>
+      <translation>Undo Delete</translation>
+    </message>
+    <message>
+      <source>Confirm deletion</source>
+      <translation>Confirm deletion</translation>
+    </message>
+    <message>
+      <source>Click to close</source>
+      <translation>Click here to close this window</translation>
+    </message>
+    <message>
+      <source>Click to open search engine</source>
+      <translation>Click to open the search engine</translation>
+    </message>
+    <message>
+      <source>Search</source>
+      <translation>Search</translation>
+    </message>
+    <message>
+      <source>Click to browse contents</source>
+      <translation>Click here to browse the content tree</translation>
+    </message>
+    <message>
+      <source>Browse</source>
+      <translation>Browse</translation>
+    </message>
+    <message>
+      <source>Create new</source>
+      <translation>Create new</translation>
+    </message>
+    <message>
+      <source>Create</source>
+      <translation>Create</translation>
+    </message>
+    <message>
+      <source>All contents</source>
+      <translation>All content</translation>
+    </message>
+    <message>
+      <source>Click to view parent</source>
+      <translation>Click to access the upper branch of the tree</translation>
+    </message>
+    <message>
+      <source>No contents</source>
+      <translation>No content</translation>
+    </message>
+    <message>
+      <source>Back</source>
+      <translation>Go back</translation>
+    </message>
+    <message>
+      <source>Go to previous</source>
+      <translation>Go to previous page</translation>
+    </message>
+    <message>
+      <source>Go to next</source>
+      <translation>Go to the next page </translation>
+    </message>
+    <message>
+      <source>Click to view children</source>
+      <translation>Click to access the content items</translation>
+    </message>
+    <message>
+      <source>Click to preview</source>
+      <translation>View content preview</translation>
+    </message>
+    <message>
+      <source>Preview</source>
+      <translation>Preview</translation>
+    </message>
+    <message>
+      <source>Close preview</source>
+      <translation>Close preview</translation>
+    </message>
+    <message>
+      <source>Add</source>
+      <translation>Add</translation>
+    </message>
+    <message>
+      <source>Selected items</source>
+      <translation>Selected items</translation>
+    </message>
+    <message>
+      <source>Remove from selection</source>
+      <translation>Remove from selected</translation>
+    </message>
+    <message>
+      <source>Add to selection</source>
+      <translation>Add to selected</translation>
+    </message>
+  </context>
+</TS>
