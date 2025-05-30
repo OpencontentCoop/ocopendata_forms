bf8cdecaefd065c2bf7fc007b9f4e85f2dd914dc
Luca Realdi <luca.realdi@opencontent.it>
2025-05-30 17:35:54 +0200

    Add por-BR and esl-ES translations

---
+++ translations/esl-ES/translation.ts
@@ -0,0 +1,111 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es">
+  <context>
+    <name>opendata_forms</name>
+    <message>
+      <source>Store</source>
+      <translation>Guardar</translation>
+    </message>
+    <message>
+      <source>Cancel</source>
+      <translation>Cancelar</translation>
+    </message>
+    <message>
+      <source>Loading...</source>
+      <translation>Cargando...</translation>
+    </message>
+    <message>
+      <source>Cancel deletion</source>
+      <translation>Cancelar eliminación</translation>
+    </message>
+    <message>
+      <source>Confirm deletion</source>
+      <translation>Confirmar eliminación</translation>
+    </message>
+    <message>
+      <source>Click to close</source>
+      <translation>Haz clic para cerrar la ventana</translation>
+    </message>
+    <message>
+      <source>Click to open search engine</source>
+      <translation>Haz clic para abrir el motor de búsqueda</translation>
+    </message>
+    <message>
+      <source>Search</source>
+      <translation>Buscar</translation>
+    </message>
+    <message>
+      <source>Click to browse contents</source>
+      <translation>Haz clic para navegar por el árbol de contenidos</translation>
+    </message>
+    <message>
+      <source>Browse</source>
+      <translation>Navegar</translation>
+    </message>
+    <message>
+      <source>Create new</source>
+      <translation>Crear nuevo</translation>
+    </message>
+    <message>
+      <source>Create</source>
+      <translation>Crear</translation>
+    </message>
+    <message>
+      <source>All contents</source>
+      <translation>Todos los contenidos</translation>
+    </message>
+    <message>
+      <source>Click to view parent</source>
+      <translation>Haz clic para acceder a la rama superior del árbol</translation>
+    </message>
+    <message>
+      <source>No contents</source>
+      <translation>Ningún contenido</translation>
+    </message>
+    <message>
+      <source>Back</source>
+      <translation>Volver atrás</translation>
+    </message>
+    <message>
+      <source>Go to previous</source>
+      <translation>Ir a la página anterior</translation>
+    </message>
+    <message>
+      <source>Go to next</source>
+      <translation>Ir a la página siguiente</translation>
+    </message>
+    <message>
+      <source>Click to view children</source>
+      <translation>Haz clic para acceder a los elementos contenidos</translation>
+    </message>
+    <message>
+      <source>Click to preview</source>
+      <translation>Visualizar la vista previa del contenido</translation>
+    </message>
+    <message>
+      <source>Preview</source>
+      <translation>Avance</translation>
+    </message>
+    <message>
+      <source>Close preview</source>
+      <translation>Cerrar vista previa</translation>
+    </message>
+    <message>
+      <source>Add</source>
+      <translation>Añadir</translation>
+    </message>
+    <message>
+      <source>Selected items</source>
+      <translation>Elementos seleccionados</translation>
+    </message>
+    <message>
+      <source>Remove from selection</source>
+      <translation>Eliminar de la selección</translation>
+    </message>
+    <message>
+      <source>Add to selection</source>
+      <translation>Añadir a la selección</translation>
+    </message>
+  </context>
+</TS>
---
+++ translations/por-BR/translation.ts
@@ -0,0 +1,111 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt-br">
+  <context>
+    <name>opendata_forms</name>
+    <message>
+      <source>Store</source>
+      <translation>Salvar</translation>
+    </message>
+    <message>
+      <source>Cancel</source>
+      <translation>Cancelar</translation>
+    </message>
+    <message>
+      <source>Loading...</source>
+      <translation>Carregando…</translation>
+    </message>
+    <message>
+      <source>Cancel deletion</source>
+      <translation>Cancelar exclusão</translation>
+    </message>
+    <message>
+      <source>Confirm deletion</source>
+      <translation>Confirmar exclusão</translation>
+    </message>
+    <message>
+      <source>Click to close</source>
+      <translation>Clique para fechar a janela</translation>
+    </message>
+    <message>
+      <source>Click to open search engine</source>
+      <translation>Clique para abrir o mecanismo de busca</translation>
+    </message>
+    <message>
+      <source>Search</source>
+      <translation>Buscar</translation>
+    </message>
+    <message>
+      <source>Click to browse contents</source>
+      <translation>Clique para navegar na árvore de conteúdos</translation>
+    </message>
+    <message>
+      <source>Browse</source>
+      <translation>Navegar</translation>
+    </message>
+    <message>
+      <source>Create new</source>
+      <translation>Criar novo</translation>
+    </message>
+    <message>
+      <source>Create</source>
+      <translation>Criar</translation>
+    </message>
+    <message>
+      <source>All contents</source>
+      <translation>Todos os conteúdos</translation>
+    </message>
+    <message>
+      <source>Click to view parent</source>
+      <translation>Clique para acessar o ramo superior da árvore</translation>
+    </message>
+    <message>
+      <source>No contents</source>
+      <translation>Nenhum conteúdo</translation>
+    </message>
+    <message>
+      <source>Back</source>
+      <translation>Voltar</translation>
+    </message>
+    <message>
+      <source>Go to previous</source>
+      <translation>Ir para a página anterior</translation>
+    </message>
+    <message>
+      <source>Go to next</source>
+      <translation>Ir para a próxima página</translation>
+    </message>
+    <message>
+      <source>Click to view children</source>
+      <translation>Clique para acessar os elementos de conteúdo</translation>
+    </message>
+    <message>
+      <source>Click to preview</source>
+      <translation>Visualizar a prévia do conteúdo</translation>
+    </message>
+    <message>
+      <source>Preview</source>
+      <translation>Prévia</translation>
+    </message>
+    <message>
+      <source>Close preview</source>
+      <translation>Fechar prévia</translation>
+    </message>
+    <message>
+      <source>Add</source>
+      <translation>Adicionar</translation>
+    </message>
+    <message>
+      <source>Selected items</source>
+      <translation>Elementos selecionados</translation>
+    </message>
+    <message>
+      <source>Remove from selection</source>
+      <translation>Remover da seleção</translation>
+    </message>
+    <message>
+      <source>Add to selection</source>
+      <translation>Adicionar à seleção</translation>
+    </message>
+  </context>
+</TS>
