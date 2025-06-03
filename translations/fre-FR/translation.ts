8bf9040ec3a2c8eb6243c279f301d90c8c69dc9a
Luca Realdi <luca.realdi@opencontent.it>
2025-06-03 11:41:25 +0200

    Add fre-FR translations

---
+++ translations/fre-FR/translation.ts
@@ -0,0 +1,111 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+  <context>
+    <name>opendata_forms</name>
+    <message>
+      <source>Store</source>
+      <translation>Enregistrer</translation>
+    </message>
+    <message>
+      <source>Cancel</source>
+      <translation>Annuler</translation>
+    </message>
+    <message>
+      <source>Loading...</source>
+      <translation>Chargement en cours...</translation>
+    </message>
+    <message>
+      <source>Cancel deletion</source>
+      <translation>Annuler la suppression</translation>
+    </message>
+    <message>
+      <source>Confirm deletion</source>
+      <translation>Confirmer la suppression</translation>
+    </message>
+    <message>
+      <source>Click to close</source>
+      <translation>Cliquer pour fermer la fenêtre</translation>
+    </message>
+    <message>
+      <source>Click to open search engine</source>
+      <translation>Cliquer pour ouvrir le moteur de recherche</translation>
+    </message>
+    <message>
+      <source>Search</source>
+      <translation>Rechercher</translation>
+    </message>
+    <message>
+      <source>Click to browse contents</source>
+      <translation>Cliquer pour parcourir l’arborescence des contenus</translation>
+    </message>
+    <message>
+      <source>Browse</source>
+      <translation>Naviguer</translation>
+    </message>
+    <message>
+      <source>Create new</source>
+      <translation>Créer un nouveau</translation>
+    </message>
+    <message>
+      <source>Create</source>
+      <translation>Créer</translation>
+    </message>
+    <message>
+      <source>All contents</source>
+      <translation>Tous les contenus</translation>
+    </message>
+    <message>
+      <source>Click to view parent</source>
+      <translation>Cliquer pour accéder à la branche supérieure de l’arborescence</translation>
+    </message>
+    <message>
+      <source>No contents</source>
+      <translation>Aucun contenu</translation>
+    </message>
+    <message>
+      <source>Back</source>
+      <translation>Revenir en arrière</translation>
+    </message>
+    <message>
+      <source>Go to previous</source>
+      <translation>Aller à la page précédente</translation>
+    </message>
+    <message>
+      <source>Go to next</source>
+      <translation>Aller à la page suivante</translation>
+    </message>
+    <message>
+      <source>Click to view children</source>
+      <translation>Cliquer pour accéder aux éléments contenus</translation>
+    </message>
+    <message>
+      <source>Click to preview</source>
+      <translation>Afficher l’aperçu du contenu</translation>
+    </message>
+    <message>
+      <source>Preview</source>
+      <translation>Aperçu</translation>
+    </message>
+    <message>
+      <source>Close preview</source>
+      <translation>Fermer l’aperçu</translation>
+    </message>
+    <message>
+      <source>Add</source>
+      <translation>Ajouter</translation>
+    </message>
+    <message>
+      <source>Selected items</source>
+      <translation>Éléments sélectionnés</translation>
+    </message>
+    <message>
+      <source>Remove from selection</source>
+      <translation>Supprimer de la sélection</translation>
+    </message>
+    <message>
+      <source>Add to selection</source>
+      <translation>Ajouter à la sélection</translation>
+    </message>
+  </context>
+</TS>
