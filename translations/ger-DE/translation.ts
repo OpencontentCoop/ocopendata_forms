a41d30208e9229770096c79977123f35c85cfeb9
Luca Realdi <luca.realdi@opencontent.it>
2025-06-09 08:41:43 +0200

    Add empty ger-DE translations

---
+++ translations/ger-DE/translation.ts
@@ -0,0 +1,111 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+  <context>
+    <name>opendata_forms</name>
+    <message>
+      <source>Store</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Cancel</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Loading...</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Cancel deletion</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Confirm deletion</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Click to close</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Click to open search engine</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Search</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Click to browse contents</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Browse</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Create new</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Create</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>All contents</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Click to view parent</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>No contents</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Back</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Go to previous</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Go to next</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Click to view children</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Click to preview</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Preview</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Close preview</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Add</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Selected items</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Remove from selection</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Add to selection</source>
+      <translation></translation>
+    </message>
+  </context>
+</TS>
